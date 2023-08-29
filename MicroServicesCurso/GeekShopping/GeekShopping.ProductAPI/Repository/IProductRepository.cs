20d4262efae59f47a441bdc969dcec359905e1ec
Douglas Baltazar <douglasbaltazar1@gmail.com>
2023-08-29 14:57:38 -0300

    feat: created DTO and interface Repository of Product

---
+++ MicroServicesCurso/GeekShopping/GeekShopping.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using GeekShopping.ProductAPI.Data.DTO;
+
+namespace GeekShopping.ProductAPI.Repository
+{
+	public interface IProductRepository
+	{
+		Task<IEnumerable<ProductDTO>> FindAll();
+
+		Task<ProductDTO> FindById(long id);
+
+		Task<ProductDTO> Create(ProductDTO dto);
+
+		Task<ProductDTO> Update(ProductDTO dto);
+
+		Task<bool> DeleteById(long id);
+	}
+}
