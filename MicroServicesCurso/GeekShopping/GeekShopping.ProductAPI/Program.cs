af8e8254baf9889d1dad87b64840e3795f79bc06
Douglas Baltazar <douglasbaltazar1@gmail.com>
2023-08-31 14:18:20 -0300

    feat: config api startup

--- MicroServicesCurso/GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ MicroServicesCurso/GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -3,6 +3,7 @@ using GeekShopping.ProductAPI.Config;
 using GeekShopping.ProductAPI.Model.Context;
 using GeekShopping.ProductAPI.Repository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,14 +16,58 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
 	c.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping API" });
+	c.EnableAnnotations();
+	c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+	{
+		Description = @"Enter 'Bearer ' [space] and your token!",
+		Name = "Authorization",
+		In = ParameterLocation.Header,
+		Type = SecuritySchemeType.ApiKey,
+		Scheme = "Bearer"
+	});
+	c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+	{
+		new OpenApiSecurityScheme
+		{
+			Reference = new OpenApiReference
+			{
+				Type = ReferenceType.SecurityScheme,
+				Id = "Bearer"
+			},
+			Scheme = "oauth2",
+			Name = "Bearer",
+			In = ParameterLocation.Header
+		},
+		new List<string> ()
+	}
+	});
 });
+
 var connection = builder.Configuration.GetConnectionString("MySQLConnectionString");
 builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version())));
 IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
 builder.Services.AddSingleton(mapper);
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddAuthentication("Bearer")
+	.AddJwtBearer(options =>
+	{
+		options.Authority = "http://localhost:5092/";
+		options.TokenValidationParameters = new TokenValidationParameters
+		{
+			ValidateAudience = false
+			
+		};
 
+    });
+builder.Services.AddAuthorization(options =>
+{
+	options.AddPolicy("ApiScope", policy =>
+	{
+		policy.RequireAuthenticatedUser();
+		policy.RequireClaim("scope", "geek_shopping");
+	});
+});
 builder.Services.AddConnections();
 
 var app = builder.Build();
@@ -33,7 +78,7 @@ if (app.Environment.IsDevelopment())
 	app.UseSwagger();
 	app.UseSwaggerUI();
 }
-
+app.UseAuthentication(); 
 app.UseAuthorization();
 
 app.MapControllers();
