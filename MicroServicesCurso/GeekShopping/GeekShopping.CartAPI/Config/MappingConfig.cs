6f04e93fa7cb5af438930575243d032d05c19d28
Douglas Baltazar <douglasbaltazar1@gmail.com>
2023-09-02 17:36:53 -0300

    feat: CartAPI Microsservice

---
+++ MicroServicesCurso/GeekShopping/GeekShopping.CartAPI/Config/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using GeekShopping.CartAPI.Model;
+
+namespace GeekShopping.CartAPI.Config
+{
+	public class MappingConfig
+	{
+		public static MapperConfiguration RegisterMaps()
+		{
+			var mappingConfig = new MapperConfiguration(config =>
+			{
+				// config.CreateMap<ProductDTO, Product>();
+				// config.CreateMap<Product, ProductDTO>();
+			});
+			
+
+			return mappingConfig;
+		}
+	}
+}
